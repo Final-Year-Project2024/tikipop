c12ed3b8dbae88fd7e9ae9743fd6b1fca916045c
shibam-naskar <shibamnskar@gmail.com>
2023-07-13 15:21:44 +0530

    Updated The Ui

---
+++ lib/app/modules/views/landingPage/landing_page.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tikipap/app/data/constants/assets.dart';
+import 'package:tikipap/app/modules/views/profile/profile_view.dart';
+
+import '../home/home_view.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int selectedIndex = 0;
+  List<dynamic> pages = [
+    HomeView(),
+    Container(),
+    const ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages[selectedIndex],
+        bottomNavigationBar: ConvexAppBar(
+          backgroundColor: Colors.white,
+          activeColor: Colors.blue,
+          initialActiveIndex: selectedIndex,
+          elevation: 0,
+          style: TabStyle.fixedCircle,
+          color: Colors.blue,
+          items: [
+            TabItem(icon: SvgPicture.asset(CustomAssets.kHome), title: ''),
+            const TabItem(icon: Icons.add, title: ''),
+            TabItem(icon: SvgPicture.asset(CustomAssets.kUserIcon), title: ''),
+          ],
+          onTap: (int i) {
+            setState(() {
+              selectedIndex = i;
+            });
+          },
+        ));
+  }
+}
---
+++ lib/app/modules/views/profile/components/follow_chip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FollowChip extends StatelessWidget {
+  const FollowChip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
